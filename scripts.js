9aa672fa89ec38e2ede0ddd95ae43d8334204f23
lucastiann <lucastianlewu@gmail.com>
2025-06-10 22:25:57 -0300

    atualizacao

--- scripts.js
+++ scripts.js
@@ -6,25 +6,41 @@ const projetos = [
     imagem: "imagens/prometeu.png",
   },
   {
-    titulo: "Jogo da Velha",
-    descricao: "Jogo da velha com JavaScript puro.",
+    titulo: "Aplicativo Calculadora Ambiental",
+    descricao: "Um apllicativo feito no Processing, que calcula quantidade necessária de árvores que precisam ser plantadas para compensar a emissão de CO2 dos meio de transportes utilizados.",
+    imagem: "imagens/calculadora.jpeg",
+    
   },
   {
-    titulo: "Relógio Digital",
-    descricao: "Um relógio digital em tempo real com HTML, CSS e JS.",
+    titulo: "Esse Site",
+    descricao: "Site criado no VS Code",
   }
 ];
 
 const lista = document.getElementById('lista-projetos');
 
+
 projetos.forEach(proj => {
   const item = document.createElement('li');
-  item.innerHTML = `
-    <a href="${proj.link}" target="_blank">
-      <img src="${proj.imagem}" alt="${proj.titulo}" width="250"><br>
-      <strong>${proj.titulo}</strong>
-    </a>
-    <p>${proj.descricao}</p>
-  `;
+  let conteudo = '';
+  
+
+  // Se tiver link e imagem, a imagem vira clicável
+  if (proj.link && proj.imagem) {
+    conteudo += `
+      <a href="${proj.link}" target="_blank">
+        <img src="${proj.imagem}" alt="${proj.titulo}" width="250">
+      </a><br>
+    `;
+  } else if (proj.imagem) {
+    // Se tiver imagem mas sem link
+    conteudo += `<img src="${proj.imagem}" alt="${proj.titulo}" width="250"><br>`;
+  }
+
+  // Título fora do link
+  conteudo += `<strong>${proj.titulo}</strong>`;
+  conteudo += `<p>${proj.descricao}</p>`;
+
+  item.innerHTML = conteudo;
   lista.appendChild(item);
 });
